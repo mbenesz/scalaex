a7f97797fb705e2c9f45345dd9e196fbc21189bf
Marcin Benesz <m.benesz@elsevier.com>
2018-08-14 18:00:41 +0100

    isVovel method add

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,24 @@
 import scala.io.Source
 
 object Main {
+
+  def isVovel(phone: String, phones: Map[String,Boolean]) :Boolean = {
+    phones.contains(phone.toUpperCase())
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hey!")
+    var phones = Map[String, Boolean]()
 
-
-    val readmeText : Iterator[String] = Source.fromResource("cmudict.0.7a.phones").getLines
-    for (line <- readmeText) {
+    val phonesText : Iterator[String] = Source.fromResource("cmudict.0.7a.phones").getLines
+    for (line <- phonesText) {
       val strings = line.split("\t")
-//      println(strings)
-      val b = a(strings(0), strings(1), null, null)
-      println(b.ip)
+      phones += (strings(0) -> strings(1).equals("vowel"))
     }
+
+    print(isVovel("ay",phones) + "\n")
+
+    print(phones.mkString("\n "))
 
 
   }
--- src/main/scala/a.scala
+++
@@ -1,3 +0,0 @@
-case class a(var ip: String, var date: String, var action: String, var username: String) {
-
-}
